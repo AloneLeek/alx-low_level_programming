eb56df6544e6e18b816ab0a5620311a8db3e2748
AloneLeek <siobhan.motley@gmail.com>
2023-02-17 02:18:26 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
 /**
- *main - Entry point
- *Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	puts("\"Programming iis like building a multilingual puzzle");
-		return (0);
+	return (0);
 }
