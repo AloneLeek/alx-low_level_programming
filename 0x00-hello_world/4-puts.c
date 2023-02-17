b1c88e8f52e718de4f6c76ee9d7273e5b2f20887
AloneLeek <siobhan.motley@gmail.com>
2023-02-17 01:36:32 -0800

    Helllo, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+   /**
+    * main - Entry point
+    *
+    * Description: Prints exactly "Programming is like building a multilingual puzzle"
+    * 
+    * Return: Always 0 (Success)
+    */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle")
+  return (0);
+}
